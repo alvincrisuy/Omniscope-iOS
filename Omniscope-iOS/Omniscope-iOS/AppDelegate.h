c9cdca211b9afa2b005fead6640f370dbf026196
Alvin Cris Uy <alvincrisuy@gmail.com>
2016-01-31 19:39:53 +0800

    Insert Xcode project

---
+++ Omniscope-iOS/Omniscope-iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Omniscope-iOS
+//
+//  Created by Cris Uy on 31/01/2016.
+//  Copyright © 2016 Pancake Unlimited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
